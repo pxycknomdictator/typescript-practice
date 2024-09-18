373fd4e49f47d2fb469669d2008a1e7a4a7db233
Noman <pxycknomdictator@outlook.com>
2024-09-18 16:27:06 +0500

    define variables in ts

--- varibales/index.ts
+++ varibales/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello Typescript");
+// Typescript is like a JavaScript but with some extra staffs and super powers!
+// JavaScript is a dynamic typed language but TypeScript is a static typed language.
+
+// ====================== Example 1 ======================
+
+let user_name: string = "John"; // we clearly mentioned that user_name variable store string only
+user_name = "Alice";
+user_name = "Bill";
+
+// but we don't do this kind of stuffs like assigning number, boolean, array to a string type variable in JavaScript we can do that but not in TypeScript
+
+// This is called static type in TypeScript
+
+// un_comment bellow code
+// user_name = 99;
+// user_name = true;
+// user_name = ["Football", "Cricket", "Baseball"];
+
+// ====================== Example 2 ======================
+
+// This array only store strings datatype only
+let students: Array<string> = ["John", "Bill", "Jimmy", "Alice", "Kevin"];
+
+// we cant push different data types in array that contain only strings
+
+// un_comment bellow code
+// students.push(90);
+// students.push(false);
+// students.push({ name: "Noman", age: 69, isLoveJs: true });
+
+let isBoolean: Boolean = true;
+
+isBoolean = false;
+
+// un_comment bellow code
+// isBoolean = "Noman"; Thats why I love Typescript
+
+// ====================== summary ======================
+// In typescript world you cant change any variable to another datatype like string to number or maybe boolean to array etc
