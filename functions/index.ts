28eb263654929455bc7e54a74cc90b02f4193ef5
Noman <pxycknomdictator@outlook.com>
2024-09-21 03:20:40 +0500

    added void datatype

--- functions/index.ts
+++ functions/index.ts
@@ -10,3 +10,28 @@ console.log(sayHello("Noman"));
 
 console.log(sayHello(83));
 console.log(sayHello({ name: "Noman" }));
+
+// you can clearly see typescript does't not give me any error to solve this problem we use type annotation
+
+function sayHelloAgain(user: string) {
+  return `Hello ${user}`;
+}
+
+console.log(sayHelloAgain("Noman"));
+
+// you might be surprised if i said that we also mentioned that our function always return string, object, number , boolean you might be got it
+
+function sayHelloAgainAgain(user: string): string {
+  return `Hello ${user}`;
+}
+
+console.log(sayHelloAgainAgain("Noman"));
+
+// amazing han? but what happens if our function never return return anything so we use void datatype
+
+function greetings(): void {
+  console.log("I don't return anything");
+}
+
+greetings();
+console.log("Hello world");
