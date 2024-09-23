8f45ee5086d9fb584808472b98b1a401a8a65981
Noman <pxycknomdictator@outlook.com>
2024-09-23 16:01:42 +0500

    facing problem withot type annotation

--- type-inference/index.ts
+++ type-inference/index.ts
@@ -1,16 +1,29 @@
 // Hey everyone, this particular section is small because we talk about type annotation and type inference.
 
 // So what is type annotation?
-// Type annotation is simply about explicitly assigning a type to any variable.
+// Type annotation is simply about explicitly assigning a type to any variable. like this
+
+let isAdmin: boolean = true;
 
 // So what is type inference
 // In TypeScript, it is not necessary to explicitly assign a type to any variable; TypeScript is smart enough to handle that.
-
-// for example
-
-let isAdmin: boolean = true;
 
 let isEmployee = true;
 
 // you don't need to mention that my variable contain this data type
 
+// "So in what situations do you need to use type annotation?"
+
+let result;
+
+const fetchData = async (path: string) => {
+  try {
+    const res = await fetch(`www.example.com/${path}`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error?.message);
+  }
+};
+
+result = fetchData("/post"); // hover on this variable the type of this variable is any '_' thats why we need to use type
