52f76617bf7c8591e87ccb79417fdc780f43f069
Noman <pxycknomdictator@outlook.com>
2024-09-23 15:17:38 +0500

    problem solved by generics

--- generics/index.ts
+++ generics/index.ts
@@ -26,3 +26,22 @@ function makeOperations(element: User): User {
   return element;
 }
 makeOperations({ name: "Noman", email: "noman69@gmail.com", isAdmin: false });
+
+// As you can see, I don't know what kind of data type I'll be getting in the future, which is why I used different functions for different types.
+
+// Hey, what's the big deal? You can just use a union type, easy!, Okay let use it
+
+interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+}
+
+function returnAnyThingYouGot(element: string | number | IUser): string | number | IUser {
+  return element;
+}
+
+returnAnyThingYouGot("")
+returnAnyThingYouGot(90)
+returnAnyThingYouGot({ username: "Noman", email: "n@n.com", password: "sheee", isAdmin: false })
