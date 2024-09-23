607c9e9e78b2c956730d0da8720a0124877b565e
Noman <pxycknomdictator@outlook.com>
2024-09-23 15:53:11 +0500

    type inference

---
+++ type-inference/index.ts
@@ -0,0 +1,16 @@
+// Hey everyone, this particular section is small because we talk about type annotation and type inference.
+
+// So what is type annotation?
+// Type annotation is simply about explicitly assigning a type to any variable.
+
+// So what is type inference
+// In TypeScript, it is not necessary to explicitly assign a type to any variable; TypeScript is smart enough to handle that.
+
+// for example
+
+let isAdmin: boolean = true;
+
+let isEmployee = true;
+
+// you don't need to mention that my variable contain this data type
+
