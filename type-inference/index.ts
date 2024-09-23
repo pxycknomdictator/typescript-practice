f42ed45b0b652854549ea295e991a81d62917dce
Noman <pxycknomdictator@outlook.com>
2024-09-23 16:09:10 +0500

    solve problem with type annotation

--- type-inference/index.ts
+++ type-inference/index.ts
@@ -14,7 +14,31 @@ let isEmployee = true;
 
 // "So in what situations do you need to use type annotation?"
 
-let result;
+// let result;
+
+// const fetchData = async (path: string) => {
+//   try {
+//     const res = await fetch(`www.example.com/${path}`);
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error(error?.message);
+//   }
+// };
+
+// (async () => {
+//   result = fetchData("/post"); // hover on this variable the type of this variable is any '_' thats why we need to use type
+// })();
+
+// lets do same thing with type annotation
+
+interface IUser {
+  name: string;
+  title: string;
+  description: string;
+}
+
+let result: IUser;
 
 const fetchData = async (path: string) => {
   try {
@@ -26,4 +50,6 @@ const fetchData = async (path: string) => {
   }
 };
 
-result = fetchData("/post"); // hover on this variable the type of this variable is any '_' thats why we need to use type
+(async () => {
+  result = await fetchData("/post");
+})();
