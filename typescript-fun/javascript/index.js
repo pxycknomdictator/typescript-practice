d7e5e7efeabf9fa4ad32c1db0bf055b03cec9d6c
Noman <pxycknomdictator@outlook.com>
2024-10-26 03:14:00 +0500

    completed project

--- typescript-fun/javascript/index.js
+++ typescript-fun/javascript/index.js
@@ -1,28 +1,48 @@
 "use strict";
+// Let's make something awesome with TypeScript
 const input = document.getElementById("inputField");
 const container = document.getElementById("container");
 const btn = document.getElementById("btn");
+const lists = document.getElementsByTagName("li");
 const databaseKey = "storageAccessKey";
 function GetDataFromFakeDatabase() {
     return JSON.parse(localStorage.getItem(databaseKey) || "[]");
 }
-function StoreDataInFakeDatabase(newTask) {
-    localStorage.setItem(databaseKey, JSON.stringify(newTask));
+function StoreDataInFakeDatabase(newData) {
+    localStorage.setItem(databaseKey, JSON.stringify(newData));
+    RenderedAllTasks();
 }
 const database = GetDataFromFakeDatabase();
 function handleGenerateNewTasks() {
     let display = input.value;
     if (display.trim() === "")
         return (display = "");
+    const duplicate = database.find((task) => task.toLowerCase() === display.toLowerCase());
+    if (duplicate)
+        return (input.value = "");
     database.push(display);
     StoreDataInFakeDatabase(database);
     input.value = "";
     RenderedAllTasks();
+    AddEventsInAllElements();
+}
+function AddEventsInAllElements() {
+    const ListArray = Array.from(lists);
+    ListArray.forEach((element) => element.addEventListener("click", (event) => {
+        const ClickHTMLElement = event.target;
+        const id = Number(ClickHTMLElement.getAttribute("data-set"));
+        RemoveHTMLElementById(id);
+    }));
+}
+function RemoveHTMLElementById(id) {
+    database.splice(id, 1);
+    StoreDataInFakeDatabase(database);
+    AddEventsInAllElements();
 }
 function RenderedAllTasks() {
     container.innerHTML = "";
-    database.forEach((task) => {
-        container.innerHTML += `<li>${task}</li>`;
+    database.forEach((task, index) => {
+        container.innerHTML += `<li data-set=${index}>${task}</li>`;
     });
 }
 RenderedAllTasks();
--- typescript-fun/typescript/index.ts
+++ typescript-fun/typescript/index.ts
@@ -1,14 +1,18 @@
+// Let's make something awesome with TypeScript
+
 const input = document.getElementById("inputField") as HTMLInputElement;
 const container = document.getElementById("container") as HTMLUListElement;
 const btn = document.getElementById("btn") as HTMLButtonElement;
+const lists = document.getElementsByTagName("li");
 const databaseKey: string = "storageAccessKey";
 
 function GetDataFromFakeDatabase(): string[] {
   return JSON.parse(localStorage.getItem(databaseKey) || "[]");
 }
 
-function StoreDataInFakeDatabase(newTask: string[]): void {
-  localStorage.setItem(databaseKey, JSON.stringify(newTask));
+function StoreDataInFakeDatabase(newData: string[]): void {
+  localStorage.setItem(databaseKey, JSON.stringify(newData));
+  RenderedAllTasks();
 }
 
 const database: string[] = GetDataFromFakeDatabase();
@@ -16,19 +20,40 @@ const database: string[] = GetDataFromFakeDatabase();
 function handleGenerateNewTasks(): void | string {
   let display: string = input.value;
   if (display.trim() === "") return (display = "");
+  const duplicate = database.find(
+    (task) => task.toLowerCase() === display.toLowerCase()
+  );
+  if (duplicate) return (input.value = "");
   database.push(display);
   StoreDataInFakeDatabase(database);
   input.value = "";
   RenderedAllTasks();
+  AddEventsInAllElements();
+}
+
+function AddEventsInAllElements() {
+  const ListArray = Array.from(lists);
+  ListArray.forEach((element) =>
+    element.addEventListener("click", (event: Event): void => {
+      const ClickHTMLElement = event.target as HTMLElement;
+      const id: number = Number(ClickHTMLElement.getAttribute("data-set"));
+      RemoveHTMLElementById(id);
+    })
+  );
+}
+
+function RemoveHTMLElementById(id: number) {
+  database.splice(id, 1);
+  StoreDataInFakeDatabase(database);
+  AddEventsInAllElements();
 }
 
 function RenderedAllTasks() {
   container.innerHTML = "";
-  database.forEach((task: string) => {
-    container.innerHTML += `<li>${task}</li>`;
+  database.forEach((task: string, index: number) => {
+    container.innerHTML += `<li data-set=${index}>${task}</li>`;
   });
 }
 
 RenderedAllTasks();
-
 btn.addEventListener("click", handleGenerateNewTasks);
